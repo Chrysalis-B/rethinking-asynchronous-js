e39ba5d0edc437073468ca5a88325343af5eb6fb
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-11-27 14:28:11 +0100

    Format ex 1

--- exercises/ex1/ex1.js
+++ exercises/ex1/ex1.js
@@ -1,4 +1,4 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	const fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
@@ -8,9 +8,9 @@ function fakeAjax(url,cb) {
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -21,8 +21,8 @@ function output(text) {
 // The old-n-busted callback way
 
 function getFile(file) {
-	fakeAjax(file,function(text){
-		// what do we do here?
+	fakeAjax(file, function (text) {
+		output(text);
 	});
 }
 
