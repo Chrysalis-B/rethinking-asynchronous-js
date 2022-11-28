fa6cbcc45a5441f5500f8106688f4f881cfa1616
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-11-28 18:52:56 +0100

    Solve ex3 (promises)

--- exercises/ex3/ex3.js
+++ exercises/ex3/ex3.js
@@ -1,4 +1,4 @@
-function fakeAjax(url,cb) {
+function fakeAjax(url, cb) {
 	var fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
@@ -8,9 +8,9 @@ function fakeAjax(url,cb) {
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -20,8 +20,21 @@ function output(text) {
 // **************************************
 
 function getFile(file) {
-	// what do we do here?
+	return new Promise(resolve => {
+		fakeAjax(file, resolve);
+	});
 }
 
 // request all files at once in "parallel"
-// ???
+const file1Promise = getFile("file1");
+const file2Promise = getFile("file2");
+const file3Promise = getFile("file3");
+
+file1Promise
+	.then(output)
+	.then(() => file2Promise)
+	.then(output)
+	.then(() => file3Promise)
+	.then(output)
+	.then(() => output('Complete!'));
+
