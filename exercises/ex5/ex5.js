0999cb0feae261317e41e9691e3b8fd438629047
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-11-29 11:54:44 +0100

    Solve ex5 (promise library)

--- exercises/ex5/ex5.js
+++ exercises/ex5/ex5.js
@@ -1,16 +1,16 @@
-function fakeAjax(url,cb) {
-	var fake_responses = {
+function fakeAjax(url, cb) {
+	const fake_responses = {
 		"file1": "The first text",
 		"file2": "The middle text",
 		"file3": "The last text"
 	};
-	var randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
+	const randomDelay = (Math.round(Math.random() * 1E4) % 8000) + 1000;
 
 	console.log("Requesting: " + url);
 
-	setTimeout(function(){
+	setTimeout(function () {
 		cb(fake_responses[url]);
-	},randomDelay);
+	}, randomDelay);
 }
 
 function output(text) {
@@ -19,9 +19,14 @@ function output(text) {
 
 // **************************************
 
-function getFile(file) {
-	// what do we do here?
-}
+const getFile = ASQ.wrap(fakeAjax, { simplecb: true })
 
-// request an array of files at once in "parallel"
-// ???
+// request all files at once in "parallel"
+
+getFile("file1")
+	.val(output)
+	.seq(getFile("file2"))
+	.val(output)
+	.seq(getFile("file3"))
+	.val(output)
+	.val(() => output('Complete!'));
